21ee6136e09f238d80be0360190457a45da1057e
Kwame Bahena <kwame@informatux.net>
2017-08-05 23:12:58 -0500

    fixing code

--- vpc_peering/private_instance.tf
+++ vpc_peering/private_instance.tf
@@ -1,7 +1,7 @@
-resource "aws_instance" "ec2" {
+resource "aws_instance" "ec2_private" {
   ami = "${var.informatux_ami}"
   subnet_id = "${aws_subnet.main-private-1.id}"
-  vpc_security_group_ids = ["${aws_security_group.allow-ssh.id}"]
+  vpc_security_group_ids = ["${aws_security_group.allow-ssh-internal.id}"]
   key_name = "${var.ssh_key_name}"
   instance_type = "${var.informatux_poc_instance_size}"
 }
--- vpc_peering/securitygroup.tf
+++ vpc_peering/securitygroup.tf
@@ -20,3 +20,27 @@ resource "aws_security_group" "allow-ssh" {
     Name = "allow-ssh" 
   }
 }
+
+
+resource "aws_security_group" "allow-ssh-internal" {
+  vpc_id = "${aws_vpc.main.id}"
+  name = "allow-ssh-internal" 
+  description = "security group that allows internal ssh and all egress traffic" 
+  egress { 
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["10.0.0.0/24"]
+  }
+
+  tags {
+    Name = "allow-ssh-internal" 
+  }
+}
