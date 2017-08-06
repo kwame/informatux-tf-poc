053ba5e608a0ff389fc26523c359c09f26879361
Kwame Bahena <kwame@informatux.net>
2017-08-05 22:57:53 -0500

    further add of variables and fixing instances.tf

--- vpc_peering/instance.tf
+++ vpc_peering/instance.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "ec2" {
   ami = "${var.informatux_ami}"
-  subnet_id = "${aws_subnet.main_public[0].id}"
+  subnet_id = "${aws_subnet.main-public-1.id}"
   vpc_security_group_ids = ["${aws_security_group.allow-ssh.id}"]
-  key_name = "${var.informatux-slm}"
+  key_name = "${var.ssh_key_name}"
+  instance_type = "${var.informatux_poc_instance_size}"
 }
--- vpc_peering/variables.tf
+++ vpc_peering/variables.tf
@@ -7,3 +7,6 @@ variable "informatux_ami" {
   default = "ami-a4c7edb2"
 }
 
+variable "informatux_poc_instance_size" {
+  default = "t2.micro"
+}
