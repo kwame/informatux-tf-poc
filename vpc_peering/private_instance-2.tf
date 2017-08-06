55869c38169232921557433d1469d3f25a10d8b7
Kwame Bahena <kwame@informatux.net>
2017-08-05 23:16:04 -0500

    adding another instance to the private vpc

---
+++ vpc_peering/private_instance-2.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "ec2_private-2" {
+  ami = "${var.informatux_ami}"
+  subnet_id = "${aws_subnet.main-private-1.id}"
+  vpc_security_group_ids = ["${aws_security_group.allow-ssh-internal.id}"]
+  key_name = "${var.ssh_key_name}"
+  instance_type = "${var.informatux_poc_instance_size}"
+}
+
